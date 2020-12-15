f3a0b8cc979d2d1faee08aca13dc2e3e27064d6d
Alejandro Martínez <alexito4@gmail.com>
2020-12-15 20:28:04 +0100

    Noted set versions

--- Sources/RuneterraWallpapersDownloader/CardSets.swift
+++ Sources/RuneterraWallpapersDownloader/CardSets.swift
@@ -9,17 +9,17 @@ public struct CardSet: Hashable {
 extension CardSet {
     public static let all = [
         CardSet(
-            ref: "Set1",
+            ref: "Set1", // 1.0
             name: "Foundations",
             url: URL(string: "https://dd.b.pvp.net/latest/set1-en_us.zip")!
         ),
         CardSet(
-            ref: "Set2",
+            ref: "Set2", // 1.0
             name: "Rising Tides",
             url: URL(string: "https://dd.b.pvp.net/latest/set2-en_us.zip")!
         ),
         CardSet(
-            ref: "Set3",
+            ref: "Set3", // 1.8
             name: "Call of the Mountain",
             url: URL(string: "https://dd.b.pvp.net/latest/set3-en_us.zip")!
         ),
