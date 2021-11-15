2273378cd5ec2f75bd205175d94d709ecf25dbcf
Alejandro Martínez <alexito4@gmail.com>
2021-11-15 20:25:51 +0100

    Fetch sets remotely

--- Sources/CLI/App.swift
+++ Sources/CLI/App.swift
@@ -11,6 +11,12 @@ struct Download: AsyncParsableCommand {
     @Option(name: .customLong("set"), help: "Specify all the Card Sets you want download. Use the set index. Ex: --set 1 --set 2.")
     var sets: [CardSet] = CardSet.all
     
+    @Flag(help: "EXPERIMENTAL - Fetch card sets from Riot's API.")
+    var fetchSets: Bool = false
+    
+    @Option(help: "EXPERIMENTAL - IDs of the sets to download.")
+    var fsets: [Int] = []
+    
     @Flag(help: "If true it reads the destination folder to find the zips instead of downloading them again.")
     var skipDownload: Bool = false
     
@@ -18,8 +24,30 @@ struct Download: AsyncParsableCommand {
     var keepZips: Bool = false
         
     mutating func runAsync() async throws {
-        print("Downloading \(sets.count) card sets:")
+        if fetchSets {
+            print("Fetching card sets information...")
+            let fetchedSets = try await CardSets().fetchSets()
+            print("\(fetchedSets.count) card sets found.")
+            let setsToDownload: [CardSet]
+            if fsets.isEmpty {
+                setsToDownload = fetchedSets
+                print("Downloading all available sets")
+            } else {
+                setsToDownload = fetchedSets
+                    .filter { fsets.contains($0.number) }
+                if setsToDownload.count != fsets.count {
+                    let missingSets = fsets
+                        .filter { number in !fetchedSets.contains { $0.number == number } }
+                    print(">> Sets [\(missingSets.map(String.init).joined(separator: ","))] not found.")
+                }
+                print("Downloading requested sets")
+            }
+            dump(setsToDownload)
+            return
+        }
         
+        print("Downloading \(sets.count) card sets:")
+
         try await withThrowingTaskGroup(of: Void.self) { group in
             let progressBar = ProgressBar()
 
@@ -32,12 +60,12 @@ struct Download: AsyncParsableCommand {
                     await extract(url, destination: destination, keepZips: keepZips)
                 }
             }
-            
+
             try await group.waitForAll()
         }
-        
+
         log(sets: sets, destination: destination)
-        
+
         print("Downloaded.")
     }
 }
